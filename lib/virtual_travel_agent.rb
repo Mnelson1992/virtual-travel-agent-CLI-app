0fedda982ccfdf6872740fb776d605dc5c030bff
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-11 04:07:27 +0000

    Created a scraper class for lonely planet website

--- lib/virtual_travel_agent.rb
+++ lib/virtual_travel_agent.rb
@@ -1,6 +1,7 @@
 require_relative "./virtual_travel_agent/version"
-
-
-
 require_relative "./virtual_travel_agent/cli"
 require_relative "./virtual_travel_agent/country"
+
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
