14701b7d89819bb4710811db56358dfa9040c1bf
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-12 02:26:25 +0000

    Set up Nokogiri to test scraper

--- lib/virtual_travel_agent/scraper.rb
+++ lib/virtual_travel_agent/scraper.rb
@@ -1,5 +1,12 @@
 class VirtualTravelAgent::Scraper
-  
+
+
+  def self.scraped_info
+    doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries"))
+    #binding.pry
+
+
+  end
 
 
 
