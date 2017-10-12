7ba566f0b917cd9d125bef70d7a7ff514f9e3341
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-12 02:19:15 +0000

    Added @@all array to collect data

--- lib/virtual_travel_agent/country.rb
+++ lib/virtual_travel_agent/country.rb
@@ -1,12 +1,16 @@
 class VirtualTravelAgent::Country
   attr_accessor :name, :description
 
+  @@all = []
+
   def initialize(name, description)
     @name = name
     @description = description
+  end
+
+  def self.all
+    @@all
   end 
-
-
 
 
 end
