7d0a6312068d20a527e022d08464528847b5cdd1
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-12 02:34:58 +0000

    Completed scraper

--- lib/virtual_travel_agent/scraper.rb
+++ lib/virtual_travel_agent/scraper.rb
@@ -5,11 +5,10 @@ class VirtualTravelAgent::Scraper
     doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries"))
     #binding.pry
     doc.css(".marketing-article").each do |destination|
-      name =
-      Description =
-    end 
-
-
+      name = destination.css("h1").text
+      description = destination.css("p").first.text.strip
+      VirtualTravelAgent::Country.new(name, description)
+    end
   end
 
 
