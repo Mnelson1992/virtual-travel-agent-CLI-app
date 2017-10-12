5c0a7cef263bb8ee7e48c8efee8763cd51498c6b
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-12 02:30:03 +0000

    Added iteration structure to scraper

--- lib/virtual_travel_agent/scraper.rb
+++ lib/virtual_travel_agent/scraper.rb
@@ -4,6 +4,10 @@ class VirtualTravelAgent::Scraper
   def self.scraped_info
     doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries"))
     #binding.pry
+    doc.css(".marketing-article").each do |destination|
+      name =
+      Description =
+    end 
 
 
   end
