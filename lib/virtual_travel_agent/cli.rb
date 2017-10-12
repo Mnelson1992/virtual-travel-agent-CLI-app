290e959f0871bc16541063973dab3518f14b16e8
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-10-12 01:55:47 +0000

    Added basic input and conditional statements into menu option

--- lib/virtual_travel_agent/cli.rb
+++ lib/virtual_travel_agent/cli.rb
@@ -20,11 +20,21 @@ class VirtualTravelAgent::CLI
 
   def menu
     input = nil
-    while input !=nil
+    while input != 'exit'
     puts "Please choose from the following menu:"
     puts "If you see a country you are interested in type in it's number"
     puts "If you would like to see a list of the countries again, type 'list'"
     puts "If you would like to exit, simply type 'exit'"
+
+    input = gets.strip
+
+    if input == 'list'
+      list
+    elsif input == (1..10)
+      puts "Here is a basic description"
+    else
+      puts "please enter a number 1 - 10, or type 'list' to see the list again or 'exit' to exit."
+    end
     end
 
   end
